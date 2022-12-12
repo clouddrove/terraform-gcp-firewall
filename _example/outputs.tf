f3df278ce948f85ac9624d11fb728765e22b236c
theprashantyadav <theprashantyadav7@gmail.com>
2022-12-12 21:48:42 +0530

    update github-action

--- _example/outputs.tf
+++ _example/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   value       = module.firewall.*.id
   description = "Name of the resource. Provided by the client when the resource is created."
 }
+
+output "self_link" {
+  value       = module.firewall.*.self_link
+  description = "The URI of the created resource."
+}
